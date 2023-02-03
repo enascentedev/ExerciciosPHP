b7318baebd4c22ab09ea68a4f45788ef8e79709b
enascentedev <106539519+enascentedev@users.noreply.github.com>
2023-02-03 19:46:06 -0300

    exercicios
    adicionei outros exercícios

---
+++ exercicio7.php
@@ -0,0 +1,13 @@
+7) Faça um algoritmo em PHP onde verifica se o valor da variável A é maior ou menor que o valor da variável B.
+A mensagem a ser impressa deve ser “A maior que B” ou “A menor que B”.
+
+<?php
+  $numA = 110;
+  $numB = 12;
+
+  if($numA > $numB){
+    echo "A maior que B";
+  }else{
+    echo "A menor que B";
+  }
+?>
